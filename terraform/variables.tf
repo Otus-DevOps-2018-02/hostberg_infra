9cb4400dda9c43046424a389df5feedb9d4313d8
Matvey Mikhaylov <hostberg@imap.cc>
2018-04-21 21:53:05 +0200

    Added gsc usage exapmle

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,25 +6,3 @@ variable region {
   description = "Region for the project"
   default     = "europe-west4"
 }
-
-variable zone {
-  description = "Zone for the project"
-  default     = "europe-west4-a"
-}
-
-variable public_key_path {
-  description = "Path to the public key used for ssh access"
-}
-
-variable private_key_path {
-  description = "Path to the private key used for ssh access"
-}
-
-variable disk_image {
-  description = "Disk image"
-}
-
-variable count {
-  description = "Number of instances"
-  default     = "1"
-}
